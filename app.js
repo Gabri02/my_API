339c147f40603ab15efc4eccbe6dcd069a9b45e6
Gabriel Sesta <gabrielsesta3@gmail.com>
2024-03-03 11:23:57 +0100

    creazione progetto, setup repository e installazione di node, creazione e setup app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const userRoutes = require("userRoutes");
+const goalsIntervalsRoutes = require("goalsIntervalsRoutes");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/intervals", goalsIntervalsRoutes);
+
+app.get("/", async (req, res) => {
+  res.send("Welcome!");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(port, () => {
+  console.log(`Server online on ${port} port`);
+});
